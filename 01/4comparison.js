949d63cceeeb8f85f4af6eabc522a3a273c1d5e9
piyushmishh <piyush.mishra2107@gamil.com>
2025-03-21 21:18:45 +0530

    4changes

---
+++ 01/4comparison.js
@@ -0,0 +1,20 @@
+// > >=< <= == !=
+
+console.log("2">1);
+console.log("02">1);
+
+// here js does automatic conversion of string to number but it can give errors smwhere
+// thus it is advised to COMPARE SAME DATA TYPES
+
+console.log(null >0);//or >= ==
+// == and >=or> works differently as compared to each other
+//for undefined it can be checked too wrt to 0
+ 
+/*
+FOR STRICT CHECK -> ==== CHECKS alue as well as datatype
+*/
+
+console.log("2"===2);
+
+// it is very complicated so try to write clean code that is compare similar data types 
+
