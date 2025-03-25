b38092ae0eb71927981e7b10c05e3e000f671f90
piyushmishh <piyush.mishra2107@gamil.com>
2025-03-25 18:15:16 +0530

    function done

--- 03/01_function.js
+++ 03/01_function.js
@@ -27,5 +27,40 @@ console.log(result);
 function loginusermsg(username){
     return `${username} just logged in`
 }
-console.log(loginusermsg("piyush"));//if piyush wasnt passed undefined logged in was o/p
+//console.log(loginusermsg("piyush"));//if piyush wasnt passed undefined logged in was o/p
+
+//how to  manage infinite parameters
+//like in shopping kart to keep on adding product cose being put in the cart
+// //function calculatecartprice(...num1){  ///...->rest operator and spread op
+//     return num1
+// }
+function calculatecartprice(val1,val2,...num1){
+    return num1
+}//200 goes to val1,900 to val2,800 and 1000 to num1
+
+console.log(calculatecartprice(200,900,800,1000));
+
+//object passed in function
+const user={
+    username:"piyu",
+    price:900
+}
+function handleobj(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleobj(user)
+//or
+handleobj({
+    username:"samu",
+    price:90000
+})
+
+//array passed in object
+const mynewarray=[900,800,700,600]
+function return2ndvalue(array){
+    return array[1]
+}
+console.log(return2ndvalue(mynewarray));
+
 
