2b0c5eb8e73ff0a64aad62badec8fa0e422a5dca
piyushmishh <piyush.mishra2107@gamil.com>
2025-03-25 22:44:49 +0530

    iife done

---
+++ 03/04_iife.js
@@ -0,0 +1,13 @@
+//immediately invoked function expression->no pollution from global scope , to protect it from pollution of global scope
+
+(function chai(){
+    //named iife
+    console.log(`db connected`);        //if we apply a parenthesis it automatically becomes a block
+    
+})(); //now its is getting executed , fn getting wrapped in ()
+//if we envoke iife then we must remember to end it ; normally done by code editor but still
+((name)=>{
+    //simple iife
+    console.log(`db connected 2 ${name}`);//using arrow function
+    
+})("piyush")
