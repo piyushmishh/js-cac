3756d3963014e812e555704dc598b583d9f853cf
piyushmishh <piyush.mishra2107@gamil.com>
2025-03-28 17:06:23 +0530

    done

---
+++ 05_iterations/six.js
@@ -0,0 +1,27 @@
+const coding=['js','ruby','java','python']
+
+// const values= coding.forEach((item)=>{
+//      console.log(item);
+//      return item //it gives undefined -> foreach loop does not return any type
+     
+// })
+
+// console.log(values);
+
+const mynums=[1,2,3,4,5,6,7,8,9,10]
+
+//using filter to return values
+// const values=mynums.filter((num)=> num>4);//filter operation returns the values //mow when {} is used outside num>4 it returns an empty array why? bcoz {} starts a scope so for that work write |return num>4|
+// console.log(values);//filter opn returned values in values array and on printing it return an array
+
+// const newnums=[]
+// mynums.forEach((num)=>{
+//       if(num>4){
+//         newnums.push(num)   //for returning or storing values using foreach loop
+//        }
+// })
+
+// console.log(newnums);
+
+// more on his code 
+
